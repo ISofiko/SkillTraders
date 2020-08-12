00cc2b1d8dd360f07251a7c78b5b893cc4418c15
Sofia Ilina <sofia.ilina@mail.utoronto.ca>
2020-08-11 20:42:43 -0400

    continuing attempts to deploy to heroku...

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import Routes from './pages/Routes';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Routes />
-	</React.StrictMode>,
+		<Routes />,
 	document.getElementById('root')
 );
 
