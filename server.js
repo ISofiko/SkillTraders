ad95b8e9046552ffce21ab04a57b2a8bc0568871
Dark Steal <darksteal132@gmail.com>
2020-08-14 13:11:02 -0400

    Finished messaging (might tweak frontend later?)

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(mongoURI, {
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
